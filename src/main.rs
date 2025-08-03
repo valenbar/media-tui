63fe482c61f5b27063e229e9ac365ac9c0097c10
Valentin <valentin.bartschies@gmail.com>
2025-08-03 23:50:14 +0200

    some refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,152 +1,30 @@
-#![allow(dead_code)]
+use color_eyre::{
+    Result,
+    eyre::{WrapErr, bail},
+};
 
-use std::io::{Cursor, Write};
-use std::process::{Command, Stdio};
-use std::{error::Error, fmt::Display};
-
-use crossterm::event::{self, Event};
-use image::{DynamicImage, ImageOutputFormat, load_from_memory};
-use lofty::prelude::*;
-use lofty::probe::Probe;
 use mpd::Client;
-use rascii_art::{self};
-use ratatui::{Frame, text::Text};
+
+mod song;
+
+#[cfg(test)]
+mod tests;
 
 const MUSIC_LIBRARY: &str = "/mnt/Volume/music-library/Music";
 
-struct Song {
-    title: String,
-    artist: String,
-    album: String,
-    cover: DynamicImage,
-}
-
-impl Song {
-    fn from_mpd(mpd_conn: &mut Client, music_library: String) -> Self {
-        let current_song = mpd_conn
-            .currentsong()
-            .expect("Failed to get current song")
-            .unwrap();
-
-        let file = music_library + "/" + &current_song.file;
-
-        let tagged_file = Probe::open(file)
-            .expect("ERROR: Bad path provided!")
-            .read()
-            .expect("ERROR: Failed to read file!");
-
-        let tag = match tagged_file.primary_tag() {
-            Some(primary_tag) => primary_tag,
-            None => tagged_file.first_tag().expect("ERROR: No tags found!"),
-        };
-
-        let album = tag.album().as_deref().unwrap_or("None").to_owned();
-
-        let cover = tag.pictures()[0].clone();
-        let cover = load_from_memory(cover.data()).expect("ERROR: Failed to load cover");
-
-        Song {
-            title: current_song.title.unwrap(),
-            artist: current_song.artist.unwrap(),
-            album,
-            cover,
-        }
-    }
-
-    fn get_cover_ascii(&self) -> Result<String, Box<dyn Error>> {
-        let mut cover_ascii = String::new();
-        let charset = self
-            .album
-            .as_str()
-            .chars()
-            .map(|c| c.to_string())
-            .collect::<Vec<String>>();
-        let charset = charset.iter().map(|c| c.as_str()).collect::<Vec<&str>>();
-        rascii_art::render_image_to(
-            &self.cover,
-            &mut cover_ascii,
-            &rascii_art::RenderOptions {
-                width: Some(25),
-                colored: true,
-                // charset: &["▁", "▂", "▃", "▄", "▅", "▆", "▇"],
-                // charset: &["󰝤"],
-                // charset: &["#"],
-                // charset: &[" ", "░", "▒", "▓", "█"],
-                charset: &charset,
-                ..Default::default()
-            },
-        )
-        .expect("ERROR: Failed to render ascii image");
-        Ok(cover_ascii)
-    }
-}
-
-impl Display for Song {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "Title: {}", self.title)?;
-        writeln!(f, "Artist: {}", self.artist)?;
-        writeln!(f, "Album: {}", self.album)?;
-        writeln!(f, "{}", &self.get_cover_ascii().expect("fail"))
-    }
-}
-
-fn draw(frame: &mut Frame) {
-    let mut conn = Client::connect("localhost:6600").expect("Failed to connect to MPD server");
-    let song = Song::from_mpd(&mut conn, MUSIC_LIBRARY.to_string());
-
-    let text = Text::raw(song.get_cover_ascii().unwrap());
-    frame.render_widget(text, frame.area());
-}
-
-fn run(terminal: &mut ratatui::DefaultTerminal) -> std::io::Result<()> {
-    loop {
-        terminal.draw(draw).expect("failed to draw frame");
-        if matches!(event::read().expect("failed to read event"), Event::Key(_)) {
-            break Ok(());
-        }
-    }
-}
-
-fn print_using_chafa(image: &DynamicImage) -> Result<String, Box<dyn Error>> {
-    let mut image_buffer = Cursor::new(Vec::new());
-    image.write_to(&mut image_buffer, ImageOutputFormat::Png)?;
-    let image_bytes = image_buffer.into_inner();
-
-    let mut child = Command::new("chafa")
-        .arg("--size=25x25")
-        .arg("--format=symbols")
-        .arg("--colors=full")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("failed to execute chafa");
-
-    {
-        let mut stdin = child.stdin.take().unwrap();
-        if let Err(e) = stdin.write_all(&image_bytes) {
-            let _ = child.wait();
-            return Err(Box::new(e));
-        }
-    }
-
-    let output = child.wait_with_output()?;
-    let ascii = String::from_utf8(output.stdout)?;
-    Ok(ascii)
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
+    color_eyre::install()?;
     let mut conn = Client::connect("localhost:6600").expect("Failed to connect to MPD server");
 
-    let song = Song::from_mpd(&mut conn, MUSIC_LIBRARY.to_string());
+    let song = song::Song::from_mpd(&mut conn, MUSIC_LIBRARY.to_string());
 
     // println!("{}", song.get_cover_ascii().unwrap());
 
-    print!("{}", print_using_chafa(&song.cover)?);
+    print!(
+        "{}",
+        song::print_using_chafa(&song.cover).wrap_err("failed to generate ascii")?
+    );
     println!("{} - {}", song.artist, song.title);
-
-    // let mut terminal = ratatui::init();
-    // run(&mut terminal)?;
-    // ratatui::restore();
 
     Ok(())
 }
---
+++ src/song.rs
@@ -0,0 +1,115 @@
+use super::*;
+use image::DynamicImage;
+use image::ImageOutputFormat;
+use image::load_from_memory;
+use lofty::file::TaggedFileExt;
+use lofty::probe::Probe;
+use lofty::tag::Accessor;
+use std::fmt::Display;
+use std::io::Cursor;
+use std::io::Write;
+use std::process::Command;
+use std::process::Stdio;
+
+pub struct Song {
+    pub title: String,
+    pub artist: String,
+    pub album: String,
+    pub cover: DynamicImage,
+}
+
+impl Song {
+    pub fn from_mpd(mpd_conn: &mut Client, music_library: String) -> Self {
+        let current_song = mpd_conn
+            .currentsong()
+            .expect("Failed to get current song")
+            .unwrap();
+
+        let file = music_library + "/" + &current_song.file;
+
+        let tagged_file = Probe::open(file)
+            .expect("ERROR: Bad path provided!")
+            .read()
+            .expect("ERROR: Failed to read file!");
+
+        let tag = match tagged_file.primary_tag() {
+            Some(primary_tag) => primary_tag,
+            None => tagged_file.first_tag().expect("ERROR: No tags found!"),
+        };
+
+        let album = tag.album().as_deref().unwrap_or("None").to_owned();
+
+        let cover = tag.pictures()[0].clone();
+        let cover = load_from_memory(cover.data()).expect("ERROR: Failed to load cover");
+
+        Song {
+            title: current_song.title.unwrap(),
+            artist: current_song.artist.unwrap(),
+            album,
+            cover,
+        }
+    }
+
+    pub fn get_cover_ascii(&self) -> Result<String> {
+        let mut cover_ascii = String::new();
+        let charset = self
+            .album
+            .as_str()
+            .chars()
+            .map(|c| c.to_string())
+            .collect::<Vec<String>>();
+        let charset = charset.iter().map(|c| c.as_str()).collect::<Vec<&str>>();
+        rascii_art::render_image_to(
+            &self.cover,
+            &mut cover_ascii,
+            &rascii_art::RenderOptions {
+                width: Some(25),
+                colored: true,
+                // charset: &["▁", "▂", "▃", "▄", "▅", "▆", "▇"],
+                // charset: &["󰝤"],
+                // charset: &["#"],
+                // charset: &[" ", "░", "▒", "▓", "█"],
+                charset: &charset,
+                ..Default::default()
+            },
+        )
+        .expect("ERROR: Failed to render ascii image");
+        Ok(cover_ascii)
+    }
+}
+
+impl Display for Song {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Title: {}", self.title)?;
+        writeln!(f, "Artist: {}", self.artist)?;
+        writeln!(f, "Album: {}", self.album)?;
+        writeln!(f, "{}", &self.get_cover_ascii().expect("fail"))
+    }
+}
+
+pub fn print_using_chafa(image: &DynamicImage) -> Result<String> {
+    let mut image_buffer = Cursor::new(Vec::new());
+    image.write_to(&mut image_buffer, ImageOutputFormat::Png)?;
+    let image_bytes = image_buffer.into_inner();
+
+    let mut child = Command::new("chafa")
+        .arg("--size=25x25")
+        .arg("--format=symbols")
+        .arg("--colors=full")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .wrap_err("failed to execute chafa command")?;
+
+    {
+        let mut stdin = child.stdin.take().unwrap();
+        if let Err(err) = stdin.write_all(&image_bytes) {
+            let _ = child.wait();
+            bail!("failed to write image bytes: {err}");
+        }
+    }
+
+    let output = child.wait_with_output()?;
+    let ascii = String::from_utf8(output.stdout)?;
+    Ok(ascii)
+}
